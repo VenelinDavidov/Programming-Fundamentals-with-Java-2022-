8955138e7c3250bf7e5cf200b7a559161a095d2b
VenelinDavidov <114108014+VenelinDavidov@users.noreply.github.com>
2022-11-22 20:51:35 +0200

    Regular Expressions
     This is a Regular Expressions labs

---
+++ Regular Expressions/P03MatchDates.java
@@ -0,0 +1,32 @@
+package RegularExpressions_Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P03MatchDates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        String regex = "(?<day>\\d{2})(?<separator>[\\.\\-\\/])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})";
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(text);
+
+
+        while (matcher.find()) {
+
+            String day = matcher.group("day");
+            String month = matcher.group("month");
+            String year = matcher.group("year");
+
+
+            System.out.printf("Day: %s, Month: %s, Year: %s%n", day, month, year);
+
+        }
+    }
+}
