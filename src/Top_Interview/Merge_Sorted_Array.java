89fb05b772a46f828cca40b50c709bb6436fc6f9
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-12-20 23:20:07 +0200

    apply new logic and update my task

---
+++ src/Top_Interview/Merge_Sorted_Array.java
@@ -0,0 +1,42 @@
+package Top_Interview;
+
+import java.util.Arrays;
+
+
+public class Merge_Sorted_Array {
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        System.out.println("Merged Array: " + Arrays.toString(nums1));
+    }
+
+    //nums1 = [1,2,3,0,0,0]
+    //nums2 = [2,5,6]
+    //Output: [1,2,2,3,5,6]
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
---
+++ src/Top_Interview/Remove_Element.java
@@ -0,0 +1,14 @@
+package Top_Interview;
+
+import java.util.Scanner;
+
+public class Remove_Element {
+    public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+
+
+    }
+
+
+
+}
