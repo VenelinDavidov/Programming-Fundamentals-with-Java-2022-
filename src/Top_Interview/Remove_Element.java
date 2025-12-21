f0c02e9d58f66bb36c7f5409dacb41b6bed032c1
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-12-21 21:47:46 +0200

    Update task

--- src/Top_Interview/Remove_Element.java
+++ src/Top_Interview/Remove_Element.java
@@ -1,14 +1,33 @@
 package Top_Interview;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Remove_Element {
     public static void main(String[] args) {
-          Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
+
+        int[] num = {3, 2, 2, 3};
+        int val = 3;
+
+        int count = removeElement(num, val);
 
     }
 
+    private static int removeElement(int[] num, int val) {
 
+        int k = 0;
 
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] != val) {
+                num[k] = num[i];
+                k++;
+            }
+        }
+        System.out.println(k);
+        System.out.println(Arrays.toString(num));
+
+        return k;
+    }
 }
