05aa9b49b2a7d9e324888182f8db66cfc42c92da
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-12-22 22:39:37 +0200

    Update new task and decided and add new logic

--- src/Top_Interview/Remove_Duplicates.java
+++ src/Top_Interview/Remove_Duplicates.java
@@ -12,12 +12,14 @@ public class Remove_Duplicates {
           System.out.println(result);
     }
 
-   // nums = [1,1,2]
+//    Input: nums = [1,1,2]
+//    Output: 2, nums = [1,2,_]
 
     private static int removeDuplicates(int[] nums) {
 
         int k = 1;
         for (int i = 1; i < nums.length; i++) {
+
             if (nums[i] != nums[i - 1]){
               nums[k] = nums[i];
               k++;
